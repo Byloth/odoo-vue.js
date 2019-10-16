0a8db2c69b84e757663346eb9aa7276346939407
Filippo Iovine <filippo.jovine@gmail.com>
2019-10-16 14:16:00 +0200

    [web_widget_fonticonpicker] Initial release.

---
+++ web_widget_fonticonpicker/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 LevelPrime srl <filippo.iovine@levelprime.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": """Web Widget Font Icon Picker""",
+    "summary": """Add a form field widget to pick font icons (default fontawesome 4.5).""",
+    "category": "web",
+    "images": ['static/description/icon.png'],
+    "version": "10.0.1",
+    "author": "LevelPrime srl",
+    "license": "AGPL-3",
+    "website": "https://www.levelprime.com",
+    "support": "filippo.iovine@levelprime.com",
+
+    "installable": True,
+    "application": False,
+
+    "depends": [
+        "web"
+    ],
+    "data": [
+        'view/assets.xml'
+    ],
+    "qweb": [
+        'static/src/xml/web_widget_fonticonpicker.xml',
+    ]
+}
