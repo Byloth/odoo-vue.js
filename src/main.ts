54f6d4b54dd9d151a2bb7151ff1f5de227de9c96
Matteo Bilotta <mbilotta@linkgroup.it>
2019-12-23 10:54:14 +0100

    [UPD] Xmas dependencies update. 🔝 🎅🎄

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,28 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
-#
-# TODO: addons/web/static/src/js/core/rpc.js
-#
+import axios from "axios";
+
+axios.post('/web/dataset/call_kw/res.partner/browse/', {
+    params: {
+        args: [[1, 2, 3, 4], ["display_name"]],
+        kwargs: {},
+        model: 'res.partner',
+        method: 'browse'
+    }
+})
+.then(function(response)
+{
+    console.log(response);
+})
+.catch(function(error)
+{
+    console.log(error);
+});
+
+/*
+ * TODO: addons/web/static/src/js/core/rpc.js
+ */
 
 new Vue({
   router,
